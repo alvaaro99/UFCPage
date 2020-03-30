4990f23917207f8234fcf45ccc35cb645d6fca5b
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-03-30 17:49:33 +0200

    refactor database

--- server/databases/fighters/sql/details.sql
+++ server/databases/fighters/sql/details.sql
@@ -1,4 +1,4 @@
-create table detalles
+create table details
 (
     Name      text null,
     Height    text null,
--- server/databases/products/sql/products.sql
+++ server/databases/products/sql/products.sql
@@ -3,7 +3,7 @@ create table product
     id          bigint       not null
         primary key,
     description varchar(255) null,
-    image       varchar(255) null,
+    image       longtext null,
     name        varchar(255) null,
     price       float        null
 );
