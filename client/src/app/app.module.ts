fb5ba1d92f8dd22746b64ccfc5ec43eecbf1ab7e
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-03-30 19:04:02 +0200

    some components created

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './views/navbar/navbar.component';
+import { RankingComponent } from './views/ranking/ranking.component';
+import { ShopComponent } from './views/shop/shop.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, RankingComponent, ShopComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
