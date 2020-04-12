d644df5239bcbc706278ee0b99650939b951f979
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-04-12 18:01:24 +0200

    .env working

--- server/rest-gateway/src/app.module.ts
+++ server/rest-gateway/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    ConfigModule.forRoot({
+      envFilePath: './src/environments/.env',
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
