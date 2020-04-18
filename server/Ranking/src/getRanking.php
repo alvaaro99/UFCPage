a649f91ab6747fd6bb415f6165bfdbb6a36425c4
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-04-18 19:25:44 +0200

    refactor

--- server/Ranking/src/getRanking.php
+++ server/Ranking/src/getRanking.php
@@ -7,14 +7,14 @@ $env = parse_ini_file(ENV);
 
 $mongo = new MongoDB\Client('mongodb://'.$env['MONGOURL'].':'.$env['MONGOPORT']);
 
-$collection = $mongo->selectCollection('fighters','ranking');
+$collection = $mongo->selectCollection('ranking','ranking');
 
 $result = $collection->find();
 
 $ranking = [];
 
 foreach ($result as $row){
-    $ranking[] = ["name"=>$row['fighter'],"weightclass"=>$row['weightclass'],"date"=>$row["date"],"rank"=>$row['rank']];
+    $ranking[] = ["name"=>$row['fighter'],"weightclass"=>$row['weightclass'],"date"=>$row["date"],"rank"=>$row['rank'],"height"=>$row['height'],"birthdate"=>['birthdate']];
 }
 
 $ranking = json_encode($ranking);
