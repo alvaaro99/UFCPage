45e0e7fb0e162b87f89de2196276ea37d81e1112
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-04-19 17:37:56 +0200

    created ranking service

---
+++ server/rest-gateway/src/fighters/ranking/ranking.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, HttpService } from '@nestjs/common';
+import { map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable()
+export class RankingService {
+  constructor(private httpClient: HttpService) {}
+
+  getAll = () =>
+    this.httpClient.get(process.env.RANKING_URL).pipe(
+      map(response => response.data),
+      catchError(error =>
+        !error.response
+          ? throwError({ status: 404, data: 'url no encontrada' })
+          : throwError(error.response),
+      ),
+    );
+}
