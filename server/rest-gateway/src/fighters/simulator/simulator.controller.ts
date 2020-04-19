eda3784fe81cabe022c2466049752c0c749797e9
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-04-19 18:22:32 +0200

    simulator working

--- server/rest-gateway/src/fighters/simulator/simulator.controller.ts
+++ server/rest-gateway/src/fighters/simulator/simulator.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus, Post, Body } from '@nestjs/common';
+import { SimulatorService } from './simulator.service';
+import { Response } from 'express';
+import { IFighter } from '../fighter.model';
+import { IAxiosError } from 'src/exceptions/axios.error';
+import { IFightDto, IFight } from './fight.model';
 
 @Controller('simulator')
-export class SimulatorController {}
+export class SimulatorController {
+  constructor(private simulatorService: SimulatorService) {}
+
+  @Get()
+  getAll(@Res() res: Response) {
+    const fighters$ = this.simulatorService.getAllFighters();
+
+    fighters$.subscribe(
+      (fighters: IFighter[]) => res.status(HttpStatus.OK).send(fighters),
+      (error: IAxiosError) => res.status(error.status).send(error.data),
+    );
+  }
+
+  @Post()
+  simulateFight(@Body() fighters: IFightDto, @Res() res: Response) {
+    const fight$ = this.simulatorService.simulateFight(fighters);
+
+    fight$.subscribe(
+      (fight: IFight) => res.status(HttpStatus.OK).send(fight),
+      (error: IAxiosError) => res.status(error.status).send(error.data),
+    );
+  }
+}
--- server/rest-gateway/src/fighters/simulator/simulator.service.ts
+++ server/rest-gateway/src/fighters/simulator/simulator.service.ts
@@ -1,6 +1,29 @@
 import { Injectable, HttpService } from '@nestjs/common';
+import { map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { IFightDto } from './fight.model';
 
 @Injectable()
 export class SimulatorService {
   constructor(private httpClient: HttpService) {}
+
+  getAllFighters = () =>
+    this.httpClient.get(process.env.SIMULATOR_URL).pipe(
+      map(response => response.data),
+      catchError(error =>
+        !error.response
+          ? throwError({ status: 404, data: 'url no encontrada' })
+          : throwError(error.response),
+      ),
+    );
+
+  simulateFight = (fighters: IFightDto) =>
+    this.httpClient.post(process.env.SIMULATOR_URL, fighters).pipe(
+      map(response => response.data),
+      catchError(error =>
+        !error.response
+          ? throwError({ status: 404, data: 'url no encontrada' })
+          : throwError(error.response),
+      ),
+    );
 }
