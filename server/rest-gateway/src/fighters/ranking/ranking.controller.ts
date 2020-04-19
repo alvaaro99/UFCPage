383a211ffcaef3c9941e1c92d1f0945421a32abf
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-04-19 17:38:07 +0200

    ranking working

--- server/rest-gateway/src/fighters/ranking/ranking.controller.ts
+++ server/rest-gateway/src/fighters/ranking/ranking.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
+import { RankingService } from './ranking.service';
+import { IFighter } from '../fighter.model';
+import { IAxiosError } from 'src/exceptions/axios.error';
 
 @Controller('ranking')
-export class RankingController {}
+export class RankingController {
+  constructor(private rankingService: RankingService) {}
+
+  @Get()
+  getFighters(@Res() res: Response) {
+    const ranking$ = this.rankingService.getAll();
+
+    ranking$.subscribe(
+      (fighters: IFighter[]) => res.status(HttpStatus.OK).send(fighters),
+      (error: IAxiosError) => res.status(error.status).send(error.data),
+    );
+  }
+}
--- server/rest-gateway/src/fighters/ranking/ranking.module.ts
+++ server/rest-gateway/src/fighters/ranking/ranking.module.ts
@@ -1,7 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
 import { RankingController } from './ranking.controller';
+import { RankingService } from './ranking.service';
 
 @Module({
-  controllers: [RankingController]
+  imports: [HttpModule],
+  controllers: [RankingController],
+  providers: [RankingService],
 })
 export class RankingModule {}
