abe243ffac258774efab093f2d0cb475590ab7ff
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-04-20 16:56:41 +0200

    change type

--- client/src/app/shared/pipes/weight-ranking.pipe.ts
+++ client/src/app/shared/pipes/weight-ranking.pipe.ts
@@ -1,12 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { FighterRanking } from '../models/fighterRanking.model';
+import { IFighter } from '../models/fighter.model';
 
 @Pipe({
-  name: 'weight'
+  name: 'weight',
 })
 export class WeightRankingPipe implements PipeTransform {
-  transform(fighters: FighterRanking[], weight: string): FighterRanking[] {
+  transform(fighters: IFighter[], weight: string): IFighter[] {
     if (weight === 'all') return fighters;
-    return fighters.filter(fighter => fighter.weightclass === weight);
+    return fighters.filter((fighter) => fighter.weightclass === weight);
   }
 }
