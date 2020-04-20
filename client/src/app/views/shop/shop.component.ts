35ca7bb449ecebe4e96beb963476d9c47827b40e
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-04-20 18:53:24 +0200

    wip

--- client/src/app/views/shop/shop.component.ts
+++ client/src/app/views/shop/shop.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/shared/services/products/products.service';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.css']
+  styleUrls: ['./shop.component.css'],
 })
 export class ShopComponent implements OnInit {
-
-  constructor() { }
+  constructor(public productsService: ProductsService) {}
 
   ngOnInit(): void {
+    this.productsService.getAll().subscribe(
+      () => console.log('bin'),
+      (error) => console.log(error)
+    );
   }
-
 }
