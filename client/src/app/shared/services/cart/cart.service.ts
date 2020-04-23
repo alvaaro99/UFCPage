25e0397d68dd37c6196f439bdd38d026c85316ca
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-04-23 13:21:56 +0200

    cart service working

--- client/src/app/shared/services/cart/cart.service.ts
+++ client/src/app/shared/services/cart/cart.service.ts
@@ -13,19 +13,17 @@ export class CartService {
     this.isProductInCart(product)
       ? this.addQuantity(product)
       : this.firstQuantity(product);
-
-    console.log(this.cart);
   };
 
-  isProductInCart = (product: IProduct) => this.cart.includes(product);
+  private isProductInCart = (product: IProduct) => this.cart.includes(product);
 
-  getIndex = (product: IProduct) => this.cart.indexOf(product);
+  private getIndex = (product: IProduct) => this.cart.indexOf(product);
 
-  addQuantity = (product: IProduct) => {
+  private addQuantity = (product: IProduct) => {
     product.quantity++;
   };
 
-  firstQuantity = (product: IProduct) => {
+  private firstQuantity = (product: IProduct) => {
     product.quantity = 1;
     this.cart.push(product);
   };
@@ -38,5 +36,12 @@ export class CartService {
   removeProduct = (product: IProduct) => {
     const index = this.getIndex(product);
     this.cart.splice(index, 1);
+  };
+
+  calculateTotal = () => {
+    const subtotal = this.cart.map(
+      (product: IProduct) => product.price * product.quantity
+    );
+    return subtotal.reduce((acc, curr) => curr + acc);
   };
 }
