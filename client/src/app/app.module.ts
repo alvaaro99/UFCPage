a43a3b418e934f4b759eb53733a85b03c1d0e04b
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-04-23 13:21:14 +0200

    font awesome installed

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { DateToAgePipe } from './shared/pipes/date-to-age/date-to-age.pipe';
 import { ProductCardComponent } from './shared/components/product-card/product-card.component';
 import { CartComponent } from './shared/components/cart/cart.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,13 @@ import { CartComponent } from './shared/components/cart/cart.component';
     ProductCardComponent,
     CartComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    FontAwesomeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
