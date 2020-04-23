4612172b2c94bb76c485ad2124840bc65c3b388a
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-04-23 13:21:37 +0200

    cart component working

--- client/src/app/shared/components/cart/cart.component.ts
+++ client/src/app/shared/components/cart/cart.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart/cart.service';
+import { environment } from 'src/environments/environment';
+import {
+  faShoppingCart,
+  faTimesCircle,
+  faMinusCircle,
+  faPlusCircle,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  imageServerURL: string;
+  iconCart: IconDefinition;
+  iconRemove: IconDefinition;
+  iconMinus: IconDefinition;
+  IconPlus: IconDefinition;
+  constructor(public cartService: CartService) {
+    this.iconCart = faShoppingCart;
+    this.iconRemove = faTimesCircle;
+    this.iconMinus = faMinusCircle;
+    this.IconPlus = faPlusCircle;
+    this.imageServerURL = environment.PRODUCT_URL;
   }
 
+  ngOnInit(): void {}
 }
