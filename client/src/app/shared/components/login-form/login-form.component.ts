4b703d91a4888b55761038492bd2bf0085296719
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-04-27 17:52:05 +0200

    login working

--- client/src/app/shared/components/login-form/login-form.component.ts
+++ client/src/app/shared/components/login-form/login-form.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UsersService } from '../../services/users/users.service';
+import { CustomException } from '../../exceptions/custom.exception';
+import { LocalStorageService } from '../../services/localstorage/local-storage.service';
+import { map, tap } from 'rxjs/operators';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-login-form',
@@ -9,7 +13,11 @@ import { UsersService } from '../../services/users/users.service';
 })
 export class LoginFormComponent implements OnInit {
   userData: FormGroup;
-  constructor(private userService: UsersService) {
+  constructor(
+    private userService: UsersService,
+    private storageService: LocalStorageService,
+    private location: Location
+  ) {
     this.userData = new FormGroup({
       username: new FormControl(
         '',
@@ -22,6 +30,14 @@ export class LoginFormComponent implements OnInit {
   ngOnInit(): void {}
 
   login() {
-    console.log(this.userData);
+    this.userService
+      .login(this.userData.value)
+      .pipe(
+        map(({ token }: { token: string }) =>
+          this.storageService.saveToken(token)
+        ),
+        tap(() => this.location.back())
+      )
+      .subscribe({ error: ({ error }) => new CustomException(error) });
   }
 }
