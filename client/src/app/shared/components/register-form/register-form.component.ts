12ebf0879b70e4af788e6cabdb49f759dcecee34
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-04-27 18:32:30 +0200

    register in register-form

--- client/src/app/shared/components/register-form/register-form.component.ts
+++ client/src/app/shared/components/register-form/register-form.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsersService } from '../../services/users/users.service';
+import { LocalStorageService } from '../../services/localstorage/local-storage.service';
+import { Location } from '@angular/common';
+import { map, tap } from 'rxjs/operators';
+import { CustomException } from '../../exceptions/custom.exception';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.css']
+  styleUrls: ['./register-form.component.css'],
 })
 export class RegisterFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  userData: FormGroup;
+  constructor(
+    private userService: UsersService,
+    private storageService: LocalStorageService,
+    private location: Location
+  ) {
+    this.userData = new FormGroup({
+      username: new FormControl(
+        '',
+        Validators.compose([Validators.minLength(5), Validators.required])
+      ),
+      password: new FormControl('', Validators.required),
+      birthdate: new FormControl('', Validators.required),
+    });
   }
 
+  ngOnInit(): void {}
+
+  register() {
+    this.userService
+      .register(this.userData.value)
+      .pipe(
+        map(({ token }: { token: string }) =>
+          this.storageService.saveToken(token)
+        ),
+        tap(() => this.location.back())
+      )
+      .subscribe({ error: ({ error }) => new CustomException(error) });
+  }
 }
