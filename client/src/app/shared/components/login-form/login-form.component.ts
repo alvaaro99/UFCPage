ca7547a44a532b0c561b28b4d35696243f754b99
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-04-29 12:12:10 +0200

    login delegated in parent

--- client/src/app/shared/components/login-form/login-form.component.ts
+++ client/src/app/shared/components/login-form/login-form.component.ts
@@ -1,10 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { UsersService } from '../../services/users/users.service';
-import { CustomException } from '../../exceptions/custom.exception';
-import { LocalStorageService } from '../../services/localstorage/local-storage.service';
-import { map, tap } from 'rxjs/operators';
-import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-login-form',
@@ -12,12 +7,9 @@ import { Location } from '@angular/common';
   styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent implements OnInit {
+  @Output() onLogin = new EventEmitter<FormGroup>();
   userData: FormGroup;
-  constructor(
-    private userService: UsersService,
-    private storageService: LocalStorageService,
-    private location: Location
-  ) {
+  constructor() {
     this.userData = new FormGroup({
       username: new FormControl(
         '',
@@ -29,15 +21,7 @@ export class LoginFormComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  login() {
-    this.userService
-      .login(this.userData.value)
-      .pipe(
-        map(({ token }: { token: string }) =>
-          this.storageService.saveToken(token)
-        ),
-        tap(() => this.location.back())
-      )
-      .subscribe({ error: ({ error }) => new CustomException(error) });
+  loginUser() {
+    this.onLogin.emit(this.userData.value);
   }
 }
--- client/src/app/views/login/login.component.ts
+++ client/src/app/views/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/shared/services/localstorage/local-storage.service';
+import { UsersService } from 'src/app/shared/services/users/users.service';
+import { Location } from '@angular/common';
+import { map, tap } from 'rxjs/operators';
+import { CustomException } from 'src/app/shared/exceptions/custom.exception';
+import { IUser } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   registered: boolean;
-  constructor() {
+  constructor(
+    private userService: UsersService,
+    private storageService: LocalStorageService,
+    private location: Location
+  ) {
     this.registered = true;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    !this.storageService.getToken()
+      ? (this.userService.isLoggued = false)
+      : (this.userService.isLoggued = true);
+  }
+
+  login(userData: IUser) {
+    this.userService
+      .login(userData)
+      .pipe(
+        map(({ token }: { token: string }) =>
+          this.storageService.saveToken(token)
+        ),
+        tap(() => {
+          this.userService.isLoggued = true;
+          this.location.back();
+        })
+      )
+      .subscribe({ error: ({ error }) => new CustomException(error) });
+  }
 }
