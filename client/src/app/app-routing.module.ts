51255789c950601e5e6c5377d149b9684102c340
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-04-29 12:57:48 +0200

    apply guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { ShopComponent } from './views/shop/shop.component';
 import { SimulatorComponent } from './views/simulator/simulator.component';
 import { LoginComponent } from './views/login/login.component';
 import { UserComponent } from './views/user/user.component';
+import { LogguedGuard } from './shared/guards/loggued/loggued.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'ranking', component: RankingComponent },
   { path: 'shop', component: ShopComponent },
   { path: 'simulator', component: SimulatorComponent },
-  { path: 'me', component: UserComponent },
+  { path: 'me', component: UserComponent, canActivate: [LogguedGuard] },
 ];
 
 @NgModule({
