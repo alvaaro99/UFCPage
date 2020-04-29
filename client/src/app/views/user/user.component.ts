f2c06d017c58f8b7172963504925bd9c70027942
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-04-29 14:09:23 +0200

    user me work in progress

--- client/src/app/views/user/user.component.ts
+++ client/src/app/views/user/user.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/shared/services/localstorage/local-storage.service';
+import { UsersService } from 'src/app/shared/services/users/users.service';
+import { map } from 'rxjs/operators';
+import { IUser } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  public me: IUser;
+  constructor(
+    private userService: UsersService,
+    private storageService: LocalStorageService
+  ) {}
 
   ngOnInit(): void {
+    this.userService
+      .info(this.storageService.getToken())
+      .pipe(map((user: IUser) => (this.me = user)))
+      .subscribe();
   }
 
+  logout() {
+    this.storageService.removeToken();
+    window.location.reload();
+  }
 }
