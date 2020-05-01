11119eda292ddb0b412aa519f3c67ce648bff83c
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-05-01 17:41:39 +0200

    first implementation

--- client/src/app/views/user/user.component.ts
+++ client/src/app/views/user/user.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from 'src/app/shared/services/localstorage/local-storage.service';
 import { UsersService } from 'src/app/shared/services/users/users.service';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 import { IUser } from 'src/app/shared/models/user.model';
+import { throwError } from 'rxjs';
+import { CustomException } from 'src/app/shared/exceptions/custom.exception';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -13,18 +16,25 @@ export class UserComponent implements OnInit {
   public me: IUser;
   constructor(
     private userService: UsersService,
-    private storageService: LocalStorageService
-  ) {}
-
-  ngOnInit(): void {
+    private storageService: LocalStorageService,
+    private router: Router
+  ) {
     this.userService
       .info(this.storageService.getToken())
-      .pipe(map((user: IUser) => (this.me = user)))
-      .subscribe();
+      .pipe(
+        map((user: IUser) => (this.me = user)),
+        catchError((error) => {
+          this.logout();
+          return throwError(error);
+        })
+      )
+      .subscribe({ error: (error) => new CustomException(error.error) });
   }
 
+  ngOnInit(): void {}
+
   logout() {
-    this.storageService.removeToken();
-    window.location.reload();
+    this.userService.logout();
+    this.router.navigate(['/login'], { queryParams: { returnUrl: '/me' } });
   }
 }
