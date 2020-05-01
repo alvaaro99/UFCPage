052bdfd3da5dd2edaec9ad480e492fd4d21eeeb5
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-05-01 17:40:38 +0200

    logout implemented

--- client/src/app/shared/services/users/users.service.ts
+++ client/src/app/shared/services/users/users.service.ts
@@ -2,13 +2,26 @@ import { Injectable } from '@angular/core';
 import { ILoginUser, IUser } from '../../models/user.model';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { LocalStorageService } from '../localstorage/local-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
   isLoggued: boolean;
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private storageService: LocalStorageService
+  ) {
+    !this.storageService.getToken()
+      ? (this.isLoggued = false)
+      : (this.isLoggued = true);
+  }
+
+  logout = () => {
+    this.isLoggued = false;
+    this.storageService.removeToken();
+  };
 
   login = (user: ILoginUser) =>
     this.http.post(
