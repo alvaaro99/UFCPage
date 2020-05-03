e02e0a8722702989856bfaff2700a3cb30c20e7d
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-05-03 12:56:58 +0200

    user card show information

--- client/src/app/shared/components/user-card/user-card.component.ts
+++ client/src/app/shared/components/user-card/user-card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IUser } from '../../models/user.model';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.css']
+  styleUrls: ['./user-card.component.css'],
 })
 export class UserCardComponent implements OnInit {
+  @Input() user: IUser;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
