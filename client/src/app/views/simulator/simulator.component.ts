6e8670be0a81fd299cf526119abfc8d89a0b4f56
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-05-06 14:46:59 +0200

    simulator component in progress

--- client/src/app/views/simulator/simulator.component.ts
+++ client/src/app/views/simulator/simulator.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomException } from 'src/app/shared/exceptions/custom.exception';
+import { SimulatorService } from 'src/app/shared/services/simulator/simulator.service';
+import { tap } from 'rxjs/operators';
+import { IFighter } from 'src/app/shared/models/fighter.model';
 
 @Component({
   selector: 'app-simulator',
   templateUrl: './simulator.component.html',
-  styleUrls: ['./simulator.component.css']
+  styleUrls: ['./simulator.component.css'],
 })
 export class SimulatorComponent implements OnInit {
-
-  constructor() { }
+  public weightToShow: string;
+  public dateToShow: Date;
+  public redFighter: IFighter;
+  public blueFighter: IFighter;
+  constructor(public simulatorService: SimulatorService) {}
 
   ngOnInit(): void {
+    this.getAll();
   }
 
+  getAll() {
+    if (this.simulatorService.fighters.length === 0) {
+      this.simulatorService
+        .getAll()
+        .pipe(
+          tap(() => {
+            this.weightToShow = this.simulatorService.fighters[0].weightclass;
+            this.dateToShow = this.simulatorService.fighters[
+              this.simulatorService.fighters.length - 1
+            ].date;
+          })
+        )
+        .subscribe({ error: (error) => new CustomException(error.error) });
+    }
+  }
 }
