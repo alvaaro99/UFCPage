aec7209bd989714ea340684915907015465382b3
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-05-06 14:46:25 +0200

    simulator service getAll

--- client/src/app/shared/services/simulator/simulator.service.ts
+++ client/src/app/shared/services/simulator/simulator.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { IFighter } from '../../models/fighter.model';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SimulatorService {
-  fighters: IFighter[];
+  fighters: IFighter[] = [];
   constructor(private http: HttpClient) {}
 
-  getAll = () => this.http;
+  getAll = () =>
+    this.http
+      .get(environment.GATEWAY_URL + environment.ENDPOINT_SIMULATOR)
+      .pipe(map((fighters: IFighter[]) => (this.fighters = fighters)));
 }
