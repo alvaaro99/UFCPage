142cafe9764b92d18ce93baca7cdaf6d88960736
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-05-07 14:03:13 +0200

    change the return

--- server/Simulator/src/controllers/index.php
+++ server/Simulator/src/controllers/index.php
@@ -9,4 +9,9 @@ require 'simulator-controller.php';
 
 
 if($_SERVER['REQUEST_METHOD'] == 'GET') echo getFighters();
-if($_SERVER['REQUEST_METHOD'] == 'POST') echo json_encode(simulateFight());
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $fight = simulateFight();
+    $winner = $fight->winner;
+    ($winner === $fight->red) ? $loser = $fight->blue : $loser = $fight->red;
+    echo json_encode(['winner'=>$winner,'loser'=>$loser,'winBy'=>$fight->winBy]);
+}
