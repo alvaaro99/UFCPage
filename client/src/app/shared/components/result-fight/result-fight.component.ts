2577510a311453ef688b0feb3141a0b875e5bb3a
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-05-10 11:13:27 +0200

    passed resultFight to the component

--- client/src/app/shared/components/result-fight/result-fight.component.ts
+++ client/src/app/shared/components/result-fight/result-fight.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { IFight } from '../../models/fight.model';
 
 @Component({
   selector: 'app-result-fight',
   templateUrl: './result-fight.component.html',
-  styleUrls: ['./result-fight.component.css']
+  styleUrls: ['./result-fight.component.css'],
 })
 export class ResultFightComponent implements OnInit {
+  @Input() resultFight: IFight = null;
+  @Output() onNewFight = new EventEmitter();
+  @Output() onRepeatFight = new EventEmitter();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  repeatFight() {
+    this.onRepeatFight.emit('repeat');
   }
 
+  newFight() {
+    this.onNewFight.emit('new');
+  }
 }
