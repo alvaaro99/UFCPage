1adc71f14dd0c207f5fe20abdf7e967bebbbc618
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-05-10 11:33:54 +0200

    ranking module implemented

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,40 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import { RankingComponent } from './views/ranking/ranking.component';
+
 import { ShopComponent } from './views/shop/shop.component';
 import { SimulatorComponent } from './views/simulator/simulator.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { WeightRankingPipe } from './shared/pipes/weight-ranking/weight-ranking.pipe';
+
 import { FighterCardComponent } from './shared/components/fighter-card/fighter-card.component';
 import { HttpClientModule } from '@angular/common/http';
-import { DateToAgePipe } from './shared/pipes/date-to-age/date-to-age.pipe';
 import { ProductCardComponent } from './shared/components/product-card/product-card.component';
 import { CartComponent } from './shared/components/cart/cart.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { DateRankingPipe } from './shared/pipes/date-ranking/date-ranking.pipe';
-import { NoRepeatPipe } from './shared/pipes/no-repeat/no-repeat.pipe';
+import { DateToAgePipe } from './shared/pipes/date-to-age/date-to-age.pipe';
 
 import { UserComponent } from './views/user/user.component';
 import { UserCardComponent } from './shared/components/user-card/user-card.component';
 import { SelectFighterComponent } from './shared/components/select-fighter/select-fighter.component';
 import { ResultFightComponent } from './shared/components/result-fight/result-fight.component';
 import { LoginModule } from './views/login/login.module';
+import { RankingModule } from './views/ranking/ranking.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    RankingComponent,
     ShopComponent,
     SimulatorComponent,
-    WeightRankingPipe,
     FighterCardComponent,
     DateToAgePipe,
     ProductCardComponent,
     CartComponent,
-    DateRankingPipe,
-    NoRepeatPipe,
     UserComponent,
     UserCardComponent,
     SelectFighterComponent,
@@ -52,6 +47,7 @@ import { LoginModule } from './views/login/login.module';
     FormsModule,
     ReactiveFormsModule,
     LoginModule,
+    RankingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- client/src/app/views/ranking/ranking.module.ts
+++ client/src/app/views/ranking/ranking.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { RankingComponent } from '../../views/ranking/ranking.component';
+import { WeightRankingPipe } from '../../shared/pipes/weight-ranking/weight-ranking.pipe';
+import { DateRankingPipe } from '../../shared/pipes/date-ranking/date-ranking.pipe';
+import { NoRepeatPipe } from '../../shared/pipes/no-repeat/no-repeat.pipe';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    RankingComponent,
+    WeightRankingPipe,
+    DateRankingPipe,
+    NoRepeatPipe,
+  ],
+  imports: [CommonModule],
 })
-export class RankingModule { }
+export class RankingModule {}
