e7b26a64c38ba75ab0ce6a704f003864a9fa55c3
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-05-11 14:37:24 +0200

    import forms in user module

--- client/src/app/views/user/user.module.ts
+++ client/src/app/views/user/user.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { UserComponent } from '../../views/user/user.component';
 import { UserCardComponent } from '../../shared/components/user-card/user-card.component';
 import { UserRoutingModule } from './user-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [UserCardComponent, UserComponent],
-  imports: [CommonModule, UserRoutingModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, UserRoutingModule],
 })
 export class UserModule {}
