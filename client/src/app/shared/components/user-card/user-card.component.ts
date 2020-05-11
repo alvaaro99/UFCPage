495f678964e087fdaccc40b50f4031e4c0d529f3
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-05-11 14:36:33 +0200

    modify user logic

--- client/src/app/shared/components/user-card/user-card.component.ts
+++ client/src/app/shared/components/user-card/user-card.component.ts
@@ -1,14 +1,65 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { IUser } from '../../models/user.model';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { IUser, IModifyUser } from '../../models/user.model';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.css'],
 })
-export class UserCardComponent implements OnInit {
+export class UserCardComponent {
   @Input() user: IUser;
-  constructor() {}
+  changingEmail: boolean = false;
+  changingPass: boolean = false;
+  @Output() onModifyUser = new EventEmitter<IModifyUser>();
+  formNewEmail = new FormGroup({
+    newEmail: new FormControl(
+      '',
+      Validators.compose([Validators.required, Validators.email])
+    ),
+    password: new FormControl('', Validators.required),
+  });
+  formNewPassword = new FormGroup({
+    newPassword: new FormControl(
+      '',
+      Validators.compose([Validators.required, Validators.minLength(3)])
+    ),
+    password: new FormControl('', Validators.required),
+  });
 
-  ngOnInit(): void {}
+  modifyEmail() {
+    this.user.email = this.formNewEmail.value.newEmail;
+    this.onModifyUser.emit({
+      user: this.user,
+      passwordToCheck: this.formNewEmail.value.password,
+    });
+    this.formNewEmail.reset();
+    this.cancelNewEmail();
+  }
+
+  modifyPassword() {
+    this.user.password = this.formNewPassword.value.newPassword;
+    this.onModifyUser.emit({
+      user: this.user,
+      passwordToCheck: this.formNewPassword.value.password,
+    });
+    this.formNewPassword.reset();
+    this.cancelNewPassword();
+  }
+
+  cancelNewEmail() {
+    this.changingEmail = false;
+  }
+
+  cancelNewPassword() {
+    this.changingPass = false;
+  }
+
+  changeEmail() {
+    this.changingEmail = true;
+  }
+
+  changePass() {
+    this.changingPass = true;
+  }
 }
