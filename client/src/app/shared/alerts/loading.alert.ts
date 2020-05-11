38e8d973ccca0999411301d61ff4fd1ee0f9c47d
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-05-11 12:24:32 +0200

    configure loading alert

--- client/src/app/shared/alerts/loading.alert.ts
+++ client/src/app/shared/alerts/loading.alert.ts
@@ -2,7 +2,12 @@ import Swal from 'sweetalert2';
 
 export class LoadingAlert {
   constructor() {
-    Swal.fire({ title: 'Cargando', onBeforeOpen: () => Swal.showLoading() });
+    Swal.fire({
+      title: 'Cargando',
+      onBeforeOpen: () => Swal.showLoading(),
+      allowEscapeKey: false,
+      allowOutsideClick: false,
+    });
   }
 
   close() {
