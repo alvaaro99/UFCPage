3fa96b9033956f8205da6bfe9b82bbff31116fef
Alvaro Acosta <alvaro99acosta@gmail.com>
2020-06-11 13:04:48 +0200

    principio de test

--- client/src/app/shared/services/products/products.service.spec.ts
+++ client/src/app/shared/services/products/products.service.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProductsService } from './products.service';
+import { HttpClientModule } from '@angular/common/http';
+import { IProduct } from '../../models/product.model';
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientModule] });
     service = TestBed.inject(ProductsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  /* it('should be set a global products', () => {
+    const products: IProduct[] = [
+      {
+        id: 1,
+        name: 'Probe',
+        description: 'Probe',
+        image: 'image/probe',
+        price: 10,
+      },
+    ];
+
+    service.getAll();
+  });*/
 });
